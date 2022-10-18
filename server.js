e855fe428f21f5729a0c98400f74c24f76e146b8
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-10-18 18:55:51 -0500

    Added POST Route Animal(s) Endpoint

--- server.js
+++ server.js
@@ -69,6 +69,12 @@ function filterByQuery(query, animalsArray) {
     }
   });
 
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
