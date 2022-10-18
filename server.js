46a820952b93a3c716a6026fee3d37fa68906b3b
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-10-17 23:18:08 -0500

    Updated PORT function

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const { animals } = require('./data/animals');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -54,6 +56,6 @@ function filterByQuery(query, animalsArray) {
     res.json(results);
   });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
