b219de302e76f44ee9df39699ccf2fc7c4c68d85
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-10-19 12:31:48 -0500

    Added Middleware Element | POST Data Acceptance

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -78,3 +83,4 @@ app.post('/api/animals', (req, res) => {
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
+
