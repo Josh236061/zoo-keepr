a62d34b57dfe10a809baa02d94e1a46f18d2b6e1
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-10-19 13:18:53 -0500

    Added Handling Animal Creation 'JSON' GET/POST Functions

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 
+const fs = require('fs');
+
+const path = require('path');
 
 const express = require('express');
 
@@ -57,6 +60,32 @@ function filterByQuery(query, animalsArray) {
     return result;
   }
 
+  function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+  }
+
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+
   app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -74,11 +103,18 @@ function filterByQuery(query, animalsArray) {
     }
   });
 
-app.post('/api/animals', (req, res) => {
-    // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
-});
+  app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+  
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+      res.status(400).send('The animal is not properly formatted.');
+    } else {
+      const animal = createNewAnimal(req.body, animals);
+      res.json(animal);
+    }
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
