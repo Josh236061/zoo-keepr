536b2b11ff6c90a250c23db4b9ec8d73327b957d
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-10-23 02:50:35 -0700

    Updated Dataset & Functionality for Zookeepers

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
 
 router.use(animalRoutes);
+router.use(require('./zookeeperRoutes'));
 
 module.exports = router;
