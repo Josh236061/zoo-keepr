2a3d8d6c8b8dfa8729717b473f26d73262c03812
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-10-23 09:07:16 -0700

    Finalized Code

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -4,19 +4,19 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
-  
+
 router.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/animals.html'));
+  res.sendFile(path.join(__dirname, '../../public/animals.html'));
 });
-  
+
 router.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+  res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
 });
-  
+
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 module.exports = router;
